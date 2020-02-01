44241c7bdf84f84807fe6986198bd8934abc453d
Huynh Nhọn <huynhnhon.dev@gmail.com>
2020-02-01 22:01:27 +0700

    1.1.7

--- app-options/components/home/HomeCtrl.js
+++ app-options/components/home/HomeCtrl.js
@@ -1,15 +1,5 @@
 app.controller('home-controller', function ($scope) {
     $scope.hi = 'huin nhon';
-    // chrome.runtime.sendMessage({
-    //     func: 'getData',
-    //     params: {
-    //         col: 'messengerBG'
-    //     }
-    // }, function(data){
-    //     $scope.data = data;
-    //     $scope.$apply();
-    //     console.log(data);
-    // })
     // $scope.show = function(i) {
     //     alert(i.refer)
     // }
